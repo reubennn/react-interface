b6357356eedfeaa3fce12e17a474bd66a6f84e64
Reuben Smith <reuben-smith@hotmail.com>
2020-08-10 21:16:02 +0930

    Implements the Add Appointment and List Appointment sections of the interface
    * Modifies AddAppointments.js and ListAppointments.js so remove placeholder and implement the sections
    * Modifies App.js to incorporate the new sections
    * Adds public/data.json with all pet appointment data
    * Fetches the data in App.js and uses Promises to extract the data
    * Creates several states
        - myAppointments to hold the current myAppointments
        - lastIndex for an id for each appointment
        - formDisplay so that Add Appointment header hides the formDisplay
    * Adds several functions to App.js
        - toggleForm to hide add appointment form
        - addAppointment to ad a new appointment
        - deleteAppointment to delete an appointment
    * Implements AddAppointments
        - handleChange saves the state once the user has entered data into the form
        - onSubmit causes the states of each prop to be saved and stored as a new appointment

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,61 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
+import { without } from 'lodash';
+
 class App extends Component {
 
   constructor() {
     super(); // Allow you to get information from the parent component
     this.state = {
-      myName: "Reuben"
-    }
+      myAppointments: [],
+      formDisplay: false,
+      lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
   };
+
+  componentDidMount() {
+    fetch("./data.json") // After app is assembled, it will be in the same folder
+      .then(response => response.json()) // Promise
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
 
   render() {
     return (
@@ -21,10 +68,15 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                {this.state.myName}
-                <AddAppointments />
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
                 <SearchAppointments />
-                <ListAppointments />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment} />
               </div>
             </div>
           </div>
